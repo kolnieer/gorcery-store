ddea1d585026ba5636d185e0d820054d1c8b4707
kolnieer <tolentinoannenicol@gmail.com>
2024-06-26 11:17:10 +0800

    Acitvity 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:untitled2/Home.dart';
+import 'package:untitled2/pages/dashboard.dart';
+import 'package:untitled2/pages/menu.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home (),
+  routes: {
+    '/' : (context) => Dashboard(),
+    '/menu' : (context) => Menu(),
+    '/profile' : (context) => Home(),
+  },
 ));
 
 
