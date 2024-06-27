1bae4e3fbdc492e8f71a5b29c9e6144b48357ced
kolnieer <tolentinoannenicol@gmail.com>
2024-06-27 11:53:10 +0800

    final activity 2

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:untitled2/pages/dashboard.dart';
 import 'package:untitled2/pages/menu.dart';
 
 void main() => runApp(MaterialApp(
+  initialRoute: '/menu',
   routes: {
     '/' : (context) => Dashboard(),
     '/menu' : (context) => Menu(),
--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:untitled2/services/product.dart';
+import 'package:untitled2/services/menuCard.dart';
 class Menu extends StatefulWidget {
   const Menu({super.key});
 
@@ -8,10 +9,35 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  List products = <Product>[
+    Product(productName: "Coke float", price: 69.99),
+    Product(productName: "Burger Patty", price: 45.00),
+    Product(productName: "Fries", price: 49.99),
+    Product(productName: "Chicken joy", price: 57.99),
+    Product(productName: "Sundae", price: 55.99),
+  ];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('THis is Menu'),
+      appBar: AppBar(
+        backgroundColor: Colors.orange[500],
+        title: Text(
+          'Menu',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            letterSpacing: 2.0,
+          ),
+        ),
+        centerTitle: true,
+      ),
+     body: Padding(
+       padding: EdgeInsets.all(5.0),
+       child: Column(
+         children: products.map((products) =>Menucard(product: products)).toList(),
+     ),
+      ),
     );
   }
 }
