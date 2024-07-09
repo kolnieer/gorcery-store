7c2fc4a2bcf8de830ae9038ca26d3cba04fb9940
kolnieer <tolentinoannenicol@gmail.com>
2024-07-09 15:41:37 +0800

    Sign Up

--- lib/pages/Signup.dart
+++ lib/pages/Signup.dart
@@ -16,6 +16,8 @@ class _SignupState extends State<Signup> {
   String name = '';
   String email = '';
   String password = '';
+  bool _obscure = true;
+  IconData _obscureIcon = Icons.visibility_off;
 
   createAccount(User user) async{
     final response = await http.post(
@@ -100,9 +102,23 @@ class _SignupState extends State<Signup> {
                     ),
                     SizedBox(height: 30.0,),
                     TextFormField(
-                      obscureText: true,
+                      obscureText: _obscure,
                       decoration: InputDecoration(
                         label: Text('Password'),
+                        prefixIcon: Icon(Icons.lock_rounded),
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscureIcon),
+                          onPressed: (){
+                            setState(() {
+                              _obscure = !_obscure;
+                              if(_obscure){
+                                _obscureIcon = Icons.visibility_off;
+                              }else{
+                                _obscureIcon = Icons.visibility;
+                              }
+                            });
+                          },
+                        ),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(70.0)
                         )
